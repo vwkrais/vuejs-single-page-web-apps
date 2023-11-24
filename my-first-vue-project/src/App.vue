328a3a99b7db7a4ff87d3436bed7e87143aec4ba
vwkrais <vwkrais@gmail.com>
2023-11-24 15:07:32 +0100

    initial commit, initializes vue projects and implements tasks 1-3

---
+++ my-first-vue-project/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <p>Hallo Volker, <br />DU packst das!</p>
+  <HelloWorld msg="Welcome to Your Vue.js App" />
+</template>
+
+<script>
+import HelloWorld from "./components/HelloWorld.vue";
+
+export default {
+  name: "App",
+  components: {
+    HelloWorld,
+  },
+};
+</script>
+
+<style>
+@import url("./assets/reset.css");
+</style>
